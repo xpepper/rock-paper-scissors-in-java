4562b667f437ef2872bb936ca9f3638f3e93c847
Pietro Di Bello <pierodibello@gmail.com>
2022-04-09 17:33:05 +0200

    static imports

--- src/main/java/com/jayway/rps/domain/game/Game.java
+++ src/main/java/com/jayway/rps/domain/game/Game.java
@@ -9,9 +9,10 @@ import com.jayway.rps.domain.event.GameTiedEvent;
 import com.jayway.rps.domain.event.GameWonEvent;
 import com.jayway.rps.domain.event.MoveDecidedEvent;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+
+import static java.util.Arrays.asList;
 
 public class Game {
     enum State {
@@ -24,16 +25,16 @@ public class Game {
 
     public List<Event> handle(CreateGameCommand c) {
         if (state != State.notInitialized) throw new IllegalStateException(state.toString());
-        return Arrays.asList(
+        return asList(
             new GameCreatedEvent(c.gameId, c.playerEmail));
     }
 
     public List<Event> handle(MakeMoveCommand c) {
         if (State.created == state) {
-            return Arrays.asList(new MoveDecidedEvent(c.gameId, c.playerEmail, c.move));
+            return asList(new MoveDecidedEvent(c.gameId, c.playerEmail, c.move));
         } else if (State.waiting == state) {
             if (player.equals(c.playerEmail)) throw new IllegalArgumentException("Player already in game");
-            return Arrays.asList(
+            return asList(
                 new MoveDecidedEvent(c.gameId, c.playerEmail, c.move),
                 makeEndGameEvent(c.gameId, c.playerEmail, c.move));
         } else {
